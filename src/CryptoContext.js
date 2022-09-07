a54b40fbe980502a636330a6964f8a7afc016cbd
frenchkebab <tubowangin@gmail.com>
2022-09-07 20:10:54 +0900

    [#1] useState for Currency and Symbol

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -1,8 +1,29 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 const Crypto = createContext();
 
 const CryptoContext = ({ children }) => {
-  return <Crypto.Provider>{children}</Crypto.Provider>;
+  const [currency, setCurrency] = useState('USD');
+  const [symbol, setSymbol] = useState('$');
+
+  useEffect(() => {
+    if (currency === 'USD') {
+      setSymbol('$');
+    } else if (currency === 'KRW') {
+      setSymbol('₩');
+    } else {
+      setSymbol('₹');
+    }
+  }, [currency]); // whenever currency changes this runs
+
+  return (
+    <Crypto.Provider value={{ currency, symbol, setCurrency }}>
+      {children}
+    </Crypto.Provider>
+  );
 };
 
 export default CryptoContext;
+
+export const CryptoState = () => {
+  useContext(Crypto);
+};
