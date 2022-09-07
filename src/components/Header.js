bd6f1128b74aeadbe841bec7aaf5678213678462
frenchkebab <tubowangin@gmail.com>
2022-09-07 20:01:23 +0900

    Headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,63 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Container,
+  createTheme,
+  makeStyles,
+  MenuItem,
+  Select,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: 'gold',
+    fontFamily: 'Montserrat',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#fff',
+      },
+      type: 'dark',
+    },
+  });
+
   return (
-    <AppBar color='transparent' position='static'>
-      <Container>
-        <Toolbar>
-          <Typography>Crypto Hunter</Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color='transparent' position='static'>
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => navigate('/')}
+              className={classes.title}
+              variant='h6'>
+              Crypto Hunter
+            </Typography>
+            <Select
+              variant='outlined'
+              value={'USD'}
+              style={{ width: 100, height: 40 }}>
+              <MenuItem value={'USD'}>USD</MenuItem>
+              <MenuItem value={'KRW'}>KRW</MenuItem>
+              <MenuItem value={'INR'}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
