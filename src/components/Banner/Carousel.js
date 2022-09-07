d1bc7801d49c4f931618727d9cf2ec9c7249c7f9
frenchkebab <tubowangin@gmail.com>
2022-09-07 23:13:48 +0900

    [Coin gecko API #3] Fetches data from Coingecko api

---
+++ src/components/Banner/Carousel.js
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { TrendingCoins } from '../../config/api';
+import { CryptoState } from '../../CryptoContext';
+
+const useStyles = makeStyles((theme) => ({
+  carousel: {
+    height: '50%',
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
+
+const Carousel = () => {
+  const [trending, setTrending] = useState([]);
+  const classes = useStyles();
+
+  const { currency } = CryptoState();
+
+  const fetchTrendingCoins = async () => {
+    const { data } = await axios.get(TrendingCoins(currency));
+    setTrending(data);
+  };
+  console.log(trending);
+
+  useEffect(() => {
+    fetchTrendingCoins();
+  }, [currency]);
+
+  return <div className={classes.carousel}>Carousel</div>;
+};
+
+export default Carousel;
