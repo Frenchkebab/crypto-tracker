6d8bc2baaf149dcf8686a1c3cb3b6d675cc52828
frenchkebab <tubowangin@gmail.com>
2022-09-08 13:47:42 +0900

    [#4] Coins Table

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,13 @@
 import { Container, makeStyles } from '@material-ui/core';
 import React from 'react';
 import Banner from '../components/Banner/Banner';
+import CoinsTable from '../components/CoinsTable';
 
 const HomePage = () => {
   return (
     <>
       <Banner />
+      <CoinsTable />
     </>
   );
 };
