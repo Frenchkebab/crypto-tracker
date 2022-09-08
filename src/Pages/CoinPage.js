e910b1f0dc6c9b5c1911301f8c5eff51c901db15
frenchkebab <tubowangin@gmail.com>
2022-09-08 14:43:48 +0900

    [Single Coin Page #6] Adds CoinInfo Component

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import CoinInfo from '../components/CoinInfo';
+import { SingleCoin } from '../config/api';
+import { CryptoState } from '../CryptoContext';
 
 const CoinPage = () => {
-  return <div>CoinPage</div>;
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+
+    setCoin(data);
+  };
+
+  console.log(coin);
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
+  const useStyles = makeStyles(() => ({}));
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.sidebar}>{/* sidebar */}</div>
+
+      {/* chart */}
+      <CoinInfo coin={coin} />
+    </div>
+  );
 };
 
 export default CoinPage;
